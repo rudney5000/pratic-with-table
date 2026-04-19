e3622c25655eaed6b14f17e8ec3f0e4fa0ef7174
rudney5000 <rudneyngueko@gmail.com>
2026-04-20 01:06:03 +0300

    fix: remove sortable

--- src/utils/constants/columns.ts
+++ src/utils/constants/columns.ts
@@ -1,8 +1,8 @@
 import type { Column, User } from "../../types/types";
 
 export const columns: Column<User>[] = [
-  { key: 'name', label: 'Nom', sortable: true },
-  { key: 'age', label: 'Âge', sortable: true },
-  { key: 'city', label: 'Ville', sortable: true },
+  { key: 'name', label: 'Nom' },
+  { key: 'age', label: 'Âge' },
+  { key: 'city', label: 'Ville' },
 ]
   
